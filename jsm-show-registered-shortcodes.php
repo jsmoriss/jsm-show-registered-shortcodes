9491dd085d3bf188910900507218e4aac70f279e
JS Morisset <jsm@surniaulula.com>
2020-03-04 10:19:21 -0800

    v1.0.0

--- jsm-show-registered-shortcodes.php
+++ jsm-show-registered-shortcodes.php
@@ -12,7 +12,7 @@
  * Description: Simple and lightweight plugin to show all registered shortcodes under a "Registered Shortcodes" toolbar menu item.
  * Requires PHP: 5.5
  * Requires At Least: 4.0
- * Tested Up To: 5.3.2
+ * Tested Up To: 5.4
  * Version: 1.0.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
