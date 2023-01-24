d8bd4ad69dfa26883b27bc85fe2653dc7845a323
JS Morisset <jsm@surniaulula.com>
2023-01-24 10:01:16 -0800

    v2.0.0

--- jsm-show-registered-shortcodes.php
+++ jsm-show-registered-shortcodes.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Simple and lightweight plugin to show all registered shortcodes under a "Registered Shortcodes" toolbar menu item.
  * Requires PHP: 7.2
- * Requires At Least: 5.2
+ * Requires At Least: 5.4
  * Tested Up To: 6.1.1
  * Version: 2.0.0
  *
